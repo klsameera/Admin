caba50753b8df442897801692a0097968c7b1b88
klsameera <sameera@speralabs.com.au>
2021-05-24 20:29:51 +0530

    chenge

--- resources/views/Pages/Categorys/new.blade.php
+++ resources/views/Pages/Categorys/new.blade.php
@@ -26,35 +26,35 @@
 </div>
 @endsection
 @section('content')
-<div class="categorycard"> 
-        <div class="card border-primary mb-3" style="max-width: 18rem;">
+<div class="categorycard">
+    <div class="card border-primary mb-3" style="max-width: 18rem;">
         <div class="card-header">Create New Category hear </div>
         <div class="card-body text-primary">
             <form action="{{route('categories.store')}}" method="POST">
                 @csrf
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Category Title</label>
-                <input type="text" class="form-control" name="name">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Discription</label>
-                <input type="text" class="form-control" name="detail">
-            </div>
-             <button type="submit" class="btn btn-primary">Submit</button>
- 
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Category Title</label>
+                    <input type="text" class="form-control" name="name">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Discription</label>
+                    <input type="text" class="form-control" name="detail">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+
             </form>
-         </div>
         </div>
+    </div>
 </div>
 </div>
 
 @endsection
 @push('css')
 <style>
-   .categorycard {
-    margin-left: 20%;
-    
-}
-    
+    .categorycard {
+        margin-left: 20%;
+
+    }
+
 </style>
 @endpush
